178cd9158047b16096f070deacfb4a8c0be6a91b
qaz5823091 <r19991106@gmail.com>
2022-11-27 22:39:38 +0800

    add files

---
+++ my_shell.sh
@@ -0,0 +1,25 @@
+makeBackgroundProcess() {
+	sh ./abc_bg.sh &
+	ps -ef | grep "sh ./abc_bg.sh" | grep -v grep | awk -F ' ' '{print $2}' > pid.txt
+}
+
+killBackgroundProcess() {
+	if [ -f "pid.txt" ];
+	then
+		kill -9 `cat pid.txt`
+		rm pid.txt
+	fi
+	exit
+}
+
+while true
+do
+	echo -n "my_shell > "
+	read Input
+	case "$Input" in
+		"abc_fg") source ./abc_fg.sh ;;
+		"abc_bg") makeBackgroundProcess ;;
+		"hello") echo "Hello, MyShell!" ;;
+		"exit") killBackgroundProcess
+	esac
+done
